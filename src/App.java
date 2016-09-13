c9194ee3d2d6cbde77ddfdf199ecd20801f789a7
Anders W. Olsen <awo@defalt.net>
2016-09-13 15:45:54 +0200

    .

---
+++ src/App.java
@@ -0,0 +1,17 @@
+/**
+ * "Hello, World!"-program
+ * Hold 11 - 2016-09-13 - Vesion 1.0.0
+ * Java JDK 1.8.0_101
+ * Lavet af:
+ *    Anders Wiberg Olsen (s165241)
+ *    Iman Chelhi (s165228)
+ *    Troels Just Christoffersen (s120052)
+ *    Iman Chelhi (s165228)
+ *    Sebastian Tibor (s145918)
+ */
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
